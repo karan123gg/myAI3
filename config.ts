d1379d506318e944dd89f57e5ce4451343db1d7c
karan123gg <karangunner007@gmail.com>
2025-11-25 15:34:00 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -21,10 +21,10 @@ function getDateAndTime(): string {
 
 export const DATE_AND_TIME = getDateAndTime();
 
-export const AI_NAME = "AI Bot";
+export const AI_NAME = "Mr. Computer";
 export const OWNER_NAME = "Karan";
 
-export const WELCOME_MESSAGE = `Hello! I'm ${AI_NAME}, an AI assistant created by ${OWNER_NAME}.`
+export const WELCOME_MESSAGE = `Greetings! I'm ${AI_NAME}, an AI assistant created by ${OWNER_NAME}.`
 
 export const CLEAR_CHAT_TEXT = "New";
 
